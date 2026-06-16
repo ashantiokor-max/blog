8c54ee8daebb91c3955671bb7069e732359a69b9
ashantiokor-max <ashantiokor@gmail.com>
2026-06-16 23:40:07 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,14 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("admin");
 
+  eleventyConfig.addFilter("readableDate", function(dateObj) {
+    return new Date(dateObj).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    });
+  });
+
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
   });
